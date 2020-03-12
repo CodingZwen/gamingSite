52235ccaa21dcb6d6e2205dd34a6e9f25ac72fb6
Sven <kuhumwerfer@hotmail.de>
2020-03-12 01:54:30 +0100

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -3,5 +3,14 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
+c.fillStyle = "rba(255,22,22,0.1)";
 c.fillRect(100,100,100,100);
 console.log("hii");
+
+
+c.beginpath();
+c.moveTo(50,500);
+c.lineTo(100,500);
+c.lineTo(600,450);
+c.strokestyle = "#ffaa00";
+c.stroke();
